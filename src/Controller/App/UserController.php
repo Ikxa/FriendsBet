b8166bcabd3103325dce87df05b799f2c2851a65
Jordan RENARD <jord.renard@gmail.com>
2020-08-15 17:17:12 +0200

    Groupsé

--- src/Controller/App/UserController.php
+++ src/Controller/App/UserController.php
@@ -38,26 +38,46 @@ class UserController extends AbstractController
     {
         $groupFactory = new GroupBuilder();
         $group = new Group();
-        $form = $this->createForm(GroupType::class);
+        $form = $this->createForm(GroupType::class, $group);
         $user = $this->entityManager->getRepository(User::class)->find($this->getUser()->getId());
+        $userGroupName = $user->getGroupDefined()->getName();
         $form->handleRequest($request);
         
-        if ($form->isSubmitted() && $form->isValid()) {
-            $group->setToken($groupFactory->getToken());
-            $group->setCreatedAt($groupFactory->getCreatedAt());
-            $group->setIsActive($groupFactory->isActive());
-            
-            $this->entityManager->persist($group);
-            $user->setGroupDefined($group);
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
+        if (!isset($userGroupName) || empty($userGroupName)) {
+            // L'utilisateur connecté n'a pas de groupe, il peut en créer un ou en rejoindre un via un token.
+            if ($form->isSubmitted() && $form->isValid()) {
+                $group->setToken($groupFactory->getToken());
+                $group->setCreatedAt($groupFactory->getCreatedAt());
+                $group->setIsActive($groupFactory->isActive());
+                $group->setMembers(1);
+                
+                $this->entityManager->persist($group);
+                $user->setGroupDefined($group);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
+        } else {
+            // L'utilisateur connecté a un groupe donc on affiche les informations de ce groupe avec un bouton "quitter".
         }
         
         return $this->render(
             'app/groups/index.html.twig',
             [
-                'form' => $form->createView(),
+                'form' => !isset($userGroupName) ? $form->createView() : NULL,
+                'group' => isset($userGroupName) ? $user->getGroupDefined() : NULL,
             ]
         );
     }
+    
+    /**
+     * @Route("/groups/{token}/leave", name="user.groups.leave")
+     */
+    public function leaveGroup()
+    {
+    
+    }
+    
+    /*
+     * TODO : Créer une fonction qui récupère un token en url, affiche un bouton permettant de le rejoindre et défini le groupe du token à l'utilisateur
+     */
 }
