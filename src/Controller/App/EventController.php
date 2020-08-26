e2e756d2db72512b3c5724d8765ec93021aaac70
Adrien Vilette <Adviladrien@gmail.com>
2020-08-26 20:32:51 +0200

    CSS

--- src/Controller/App/EventController.php
+++ src/Controller/App/EventController.php
@@ -18,11 +18,20 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class EventController extends AbstractController
 {
+
+    /**
+     * @Route("/event/bet", name="event.bet")
+     */
+    public function bet()
+    {
+        return new Response('Coucou');
+    }
+
     /**
      * @var Football
      */
     private $api;
-    
+
     /**
      * EventController constructor.
      *
@@ -32,16 +41,16 @@ class EventController extends AbstractController
     {
         $this->api = $footballApi;
     }
-    
+
     /**
      * @Route("/events", name="event.index")
      */
     public function index(): Response
     {
         $matchs = $this->getDoctrine()
-                       ->getRepository(Match::class)
-                       ->findAll();
-        
+            ->getRepository(Match::class)
+            ->findAll();
+
         return $this->render(
             'app/event/index.html.twig',
             [
@@ -49,7 +58,7 @@ class EventController extends AbstractController
             ]
         );
     }
-    
+
     /**
      * @Route("/event/add", name="event.add")
      * @param EntityManagerInterface $em
@@ -59,33 +68,25 @@ class EventController extends AbstractController
         $match = new Match();
         $form = $this->createForm(MatchType::class, $match);
         $form->handleRequest($request);
-        
+        $form = $this->createForm(MatchType::class);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $match->setIsCustom(TRUE);
             $match->setPlayedAt(new \DateTime());
             $match->setIsOver(FALSE);
             $match->setMatchId(mt_rand());
-            
+
             $em->persist($match);
             $em->flush();
-            
+
             return $this->redirectToRoute('event.index');
         }
-        
+
         return $this->render(
             'app/event/add.html.twig',
             [
                 'form' => $form->createView(),
             ]
         );
-    }
-    
-    
-    /**
-     * @Route("/event/bet", name="event.bet")
-     */
-    public function bet()
-    {
-        return new Response('Coucou');
     }
 }
