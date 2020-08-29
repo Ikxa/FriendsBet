54322acdc789ea899be48509fddb428d6d3b8bf1
Jordan RENARD <jord.renard@gmail.com>
2020-08-29 16:44:13 +0200

    Team & pagination

---
+++ src/Entity/App/Team.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity\App;
+
+use App\Repository\App\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TeamRepository::class)
+ */
+class Team
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $logo;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $stadium;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_active;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $league;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(?string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getStadium(): ?string
+    {
+        return $this->stadium;
+    }
+
+    public function setStadium(?string $stadium): self
+    {
+        $this->stadium = $stadium;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+
+    public function getLeague(): ?int
+    {
+        return $this->league;
+    }
+
+    public function setLeague(int $league): self
+    {
+        $this->league = $league;
+
+        return $this;
+    }
+}
